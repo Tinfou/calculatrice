1d5afea99cdef59a031be0cb4789eac03a45f91e
Tinfou <144536026+Tinfou@users.noreply.github.com>
2023-10-13 12:29:43 +0200

    calculatrice V1

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+
+    declare(strict_types=1);
+
+    function verifieNombre($nombre): bool
+    {
+        if($nombre && preg_match('/^-?\d+(\.\d+)?$/',$nombre)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function verifieOperateur($signe):bool
+    {   
+        if($signe && preg_match("/[+\-\*\/]/",$signe)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function calcul (?string $nombre1 , ?string $nombre2, string $operateur) : string
+    {
+        if (verifieNombre($nombre1) && verifieNombre($nombre2) && verifieOperateur($operateur)){
+            $resultat =0;
+            eval("\$resultat = $nombre1 $operateur $nombre2;");
+            return "valiny : $resultat";
+        }
+        else{
+            return "erreur";
+        }
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php include('functions.php');?>
+<form action="index.php" method="post">
+    <p><input type="text" name="nombre1" placeholder="Nombre a"></p>
+    <p><input type="text" name="operateur" placeholder="Opérateurs"></p>
+    <p><input type="text" name="nombre2" placeholder="Nombre b"></p>
+    <p><button type="submit">Résultat</button></p>
+</form>
+
 <?php
-echo "first commit";
+    if(isset($_POST['nombre1'])&&isset($_POST['nombre2'])&&isset($_POST['operateur'])){
+        $nombre1 = $_POST['nombre1'];
+        $nombre2 = $_POST['nombre2'];
+        $operateur = $_POST['operateur'];
+        echo calcul($nombre1,$nombre2,$operateur);
+    }
 ?>
